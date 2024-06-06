aefb09ba41a0ea17c13880c7689b401ed64bc3e5
Chandula Samarasinghe <chandulas1999@gmail.com>
2024-06-06 21:16:33 +0530

    develop the firebase authentication

---
+++ lib/widgets/button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String buttonText;
+  final Color buttonColor;
+  final Function() onTap;
+
+  const CustomButton({
+    super.key,
+    required this.buttonText,
+    required this.buttonColor,
+    required this.onTap, 
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 20),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: buttonColor,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Center(
+          child: Text(
+            buttonText,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              fontSize: 22,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
