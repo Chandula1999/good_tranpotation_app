56f1b969c10615dd1a554283743ab538b76662ae
Heshara13 <nirmithaheshara527@gmail.com>
2024-06-19 22:06:01 +0530

    add folders and files

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -8,3 +8,13 @@ Color backgroundsplash = const Color.fromRGBO(33, 150, 243, 1);
 Color textColor1 = const Color(0xff353047);
 Color textColor2 = const Color(0xff6F6B7A);
 Color buttonColor = const Color.fromARGB(255, 39, 121, 227);
+
+//home page
+const primary = Color(0xFF4b68ff);
+const secondary = Color(0xFFFFE24B);
+const accent = Color(0xFFb0c7ff);
+
+//background colors
+Color light = const Color(0xFFF6F6F6);
+Color dark = const Color(0xFF272727);
+Color primaryBackground = const Color(0XFFF3F5FF);
