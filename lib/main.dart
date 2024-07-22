0c530bee9476fe41dc78d38ad7299d9a99e3496f
Chandula Samarasinghe <Chandulas1999@gmail.com>
2024-07-23 02:33:54 +0530

    changed the navigation to auth status in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-// import 'package:good_tranpotation_app/auth/auth_status.dart';
-// import 'package:good_tranpotation_app/screens/other Screens/users_selection_screen.dart';
-import 'package:good_tranpotation_app/auth/user-role.dart';
-// import 'package:good_tranpotation_app/firebase_options.dart';
-import 'firebase_options.dart';
+import 'package:good_tranpotation_app/auth/auth_status.dart';
+import 'package:good_tranpotation_app/firebase_options.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: UserRole(),
+      home: AuthStatus(),
       // home: NavigationMenu(),
     );
   }
