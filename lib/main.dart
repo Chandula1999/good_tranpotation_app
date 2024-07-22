d5261a4a9bea8d134c2b1d5db1958fd10c052e77
Chandula Samarasinghe <Chandulas1999@gmail.com>
2024-07-23 02:17:04 +0530

    removed the auth satus.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 // import 'package:good_tranpotation_app/auth/auth_status.dart';
-import 'package:good_tranpotation_app/screens/other Screens/users_selection_screen.dart';
+// import 'package:good_tranpotation_app/screens/other Screens/users_selection_screen.dart';
 import 'package:good_tranpotation_app/user-role.dart';
 // import 'package:good_tranpotation_app/firebase_options.dart';
 import 'firebase_options.dart';
